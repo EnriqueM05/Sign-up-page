43edd9584994649557a235bc3310c246f92e1077
Enrique Milo <pokequique0527@gmail.com>
2024-08-02 15:16:51 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// to do's
+/*
+
+- Create log in page w redirect from the sign up page (see in images)
+- backend (express server)
+- When credentials created display them on another page
+- favicon
+
+*/
